1d974690fbcbdfa9921ca3a32872c507dd2ae0a5
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-04-30 01:14:21 +1000

    Add raylib boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include "raylib.h"
 
 int main(int argument_count, const char *arguments[]) {
+    const auto window_width = 1280;
+    const auto window_height = 720;
+
+    InitWindow(window_width, window_height, "Match Three");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        DrawText("Test", 100, 100, 20, RAYWHITE);
+
+        EndDrawing();
+    }
+
     return 0;
 }
