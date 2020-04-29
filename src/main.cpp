ff0aef07b0cef4e40bf4a94d48a208bcd7bc3ac6
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-04-30 01:29:37 +1000

    Add basic tile rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "raylib.h"
 
 int main(int argument_count, const char *arguments[]) {
@@ -8,12 +9,53 @@ int main(int argument_count, const char *arguments[]) {
 
     SetTargetFPS(60);
 
+    enum struct TileKind {
+        None,
+        Red,
+        Green,
+        Blue
+    };
+
+    const size_t playfield_size = 10;
+
+    TileKind tiles[playfield_size][playfield_size] {};
+
+    for(size_t y = 0; y < playfield_size; y += 1) {
+        for(size_t x = 0; x < playfield_size; x += 1) {
+            tiles[y][x] = (TileKind)GetRandomValue(0, 3);
+        }
+    }
+
     while(!WindowShouldClose()) {
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
 
-        DrawText("Test", 100, 100, 20, RAYWHITE);
+        for(size_t y = 0; y < playfield_size; y += 1) {
+            for(size_t x = 0; x < playfield_size; x += 1) {
+                auto tile_kind = tiles[y][x];
+
+                if(tile_kind != TileKind::None) {
+                    Color color;
+                    switch(tile_kind) {
+                        case TileKind::Red: color = RED; break;
+                        case TileKind::Green: color = GREEN; break;
+                        case TileKind::Blue: color = BLUE; break;
+                        default: abort();
+                    }
+
+                    const int tile_size = 32;
+
+                    DrawRectangle(
+                        window_width / 2 - playfield_size * tile_size / 2 + x * tile_size,
+                        window_height / 2 - playfield_size * tile_size / 2 + y * tile_size,
+                        tile_size,
+                        tile_size,
+                        color
+                    );
+                }
+            }
+        }
 
         EndDrawing();
     }
