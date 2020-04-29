6584c99ddccbe874eb0bd69f4788ab7d99a385d0
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-04-30 02:59:42 +1000

    Add basic completion and falling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,66 @@
 #include <stdlib.h>
 #include "raylib.h"
 
+enum struct TileKind {
+    None,
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Cyan,
+    Magenta
+};
+
+const auto playfield_size = 10;
+
+static bool in_playfield(int x, int y) {
+    return x >= 0 && y >= 0 && x < playfield_size && y < playfield_size;
+}
+
+static int count_neighbours(TileKind tiles[playfield_size][playfield_size], bool counted[playfield_size][playfield_size], int x, int y, TileKind kind) {
+    counted[y][x] = true;
+
+    auto total = 1;
+
+    if(in_playfield(x + 1, y) && tiles[y][x + 1] == kind && !counted[y][x + 1]) {
+        total += count_neighbours(tiles, counted, x + 1, y, kind);
+    }
+
+    if(in_playfield(x, y + 1) && tiles[y + 1][x] == kind && !counted[y + 1][x]) {
+        total += count_neighbours(tiles, counted, x, y + 1, kind);
+    }
+
+    if(in_playfield(x - 1, y) && tiles[y][x - 1] == kind && !counted[y][x - 1]) {
+        total += count_neighbours(tiles, counted, x - 1, y, kind);
+    }
+
+    if(in_playfield(x, y - 1) && tiles[y - 1][x] == kind && !counted[y - 1][x]) {
+        total += count_neighbours(tiles, counted, x, y - 1, kind);
+    }
+
+    return total;
+}
+
+static void delete_neighbours(TileKind tiles[playfield_size][playfield_size], int x, int y, TileKind kind) {
+    tiles[y][x] = TileKind::None;
+
+    if(in_playfield(x + 1, y) && tiles[y][x + 1] == kind) {
+        delete_neighbours(tiles, x + 1, y, kind);
+    }
+
+    if(in_playfield(x, y + 1) && tiles[y + 1][x] == kind) {
+        delete_neighbours(tiles, x, y + 1, kind);
+    }
+
+    if(in_playfield(x - 1, y) && tiles[y][x - 1] == kind) {
+        delete_neighbours(tiles, x - 1, y, kind);
+    }
+
+    if(in_playfield(x, y - 1) && tiles[y - 1][x] == kind) {
+        delete_neighbours(tiles, x, y - 1, kind);
+    }
+}
+
 int main(int argument_count, const char *arguments[]) {
     const auto window_width = 1280;
     const auto window_height = 720;
@@ -9,30 +69,65 @@ int main(int argument_count, const char *arguments[]) {
 
     SetTargetFPS(60);
 
-    enum struct TileKind {
-        None,
-        Red,
-        Green,
-        Blue
-    };
-
-    const size_t playfield_size = 10;
-
     TileKind tiles[playfield_size][playfield_size] {};
 
-    for(size_t y = 0; y < playfield_size; y += 1) {
-        for(size_t x = 0; x < playfield_size; x += 1) {
-            tiles[y][x] = (TileKind)GetRandomValue(0, 3);
-        }
-    }
-
     while(!WindowShouldClose()) {
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            bool done;
+            do {
+                done = true;
+
+                for(auto reverse_y = 0; reverse_y < playfield_size; reverse_y += 1) {
+                    auto y = playfield_size - 1 - reverse_y;
+
+                    for(auto x = 0; x < playfield_size; x += 1) {
+                        auto kind = tiles[y][x];
+
+                        if(kind != TileKind::None) {
+                            bool counted[playfield_size][playfield_size] {};
+
+                            auto count = count_neighbours(tiles, counted, x, y, kind);
+
+                            if(count >= 3) {
+                                delete_neighbours(tiles, x, y, kind);
+
+                                done = false;
+                            }
+                        }
+                    }
+                }
+
+                for(auto x = 0; x < playfield_size; x += 1) {
+                    for(auto x = 0; x < playfield_size; x += 1) {
+                        for(auto offset_y = 0; offset_y < playfield_size - 1; offset_y += 1) {
+                            auto y = playfield_size - 1 - offset_y;
+
+                            if(tiles[y][x] == TileKind::None && tiles[y - 1][x] != TileKind::None) {
+                                tiles[y][x] = tiles[y - 1][x];
+                                tiles[y - 1][x] = TileKind::None;
+
+                                done = false;
+                            }
+                        }
+                    }
+
+                    tiles[0][x] = TileKind::None;
+                }
+            } while(!done);
+        } else if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+            for(auto y = 0; y < playfield_size; y += 1) {
+                for(auto x = 0; x < playfield_size; x += 1) {
+                    tiles[y][x] = (TileKind)GetRandomValue(1, 6);
+                }
+            }
+        }
+
         BeginDrawing();
 
         ClearBackground(WHITE);
 
-        for(size_t y = 0; y < playfield_size; y += 1) {
-            for(size_t x = 0; x < playfield_size; x += 1) {
+        for(auto y = 0; y < playfield_size; y += 1) {
+            for(auto x = 0; x < playfield_size; x += 1) {
                 auto tile_kind = tiles[y][x];
 
                 if(tile_kind != TileKind::None) {
@@ -41,10 +136,13 @@ int main(int argument_count, const char *arguments[]) {
                         case TileKind::Red: color = RED; break;
                         case TileKind::Green: color = GREEN; break;
                         case TileKind::Blue: color = BLUE; break;
+                        case TileKind::Yellow: color = YELLOW; break;
+                        case TileKind::Cyan: color = SKYBLUE; break;
+                        case TileKind::Magenta: color = PURPLE; break;
                         default: abort();
                     }
 
-                    const int tile_size = 32;
+                    const auto tile_size = 32;
 
                     DrawRectangle(
                         window_width / 2 - playfield_size * tile_size / 2 + x * tile_size,
